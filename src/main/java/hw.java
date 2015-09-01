d9bd901ab8b5b8831f88eacf048e3c1fe3fbca72
chuta <chuta@cisco.com>
2015-09-01 18:16:56 +0800

    +hw

---
+++ src/main/java/hw.java
@@ -0,0 +1,10 @@
+import org.apache.log4j.Logger;
+
+
+public class hw {
+	public static void main(String[] args) {
+		final Logger logger=Logger.getLogger(hw.class);
+		logger.info("Test mvn");
+	}
+
+}
