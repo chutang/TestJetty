f43ce00a58bae2c39f510919d9d06b261c9b8760
chutang <tch2008@126.com>
2015-09-02 10:50:49 +0800

    +

--- src/main/java/test/hw.java
+++ src/main/java/test/hw.java
@@ -4,8 +4,8 @@ import org.apache.log4j.Logger;
 
 public class hw {
 	public static void main(String[] args) {
-//		final Logger logger=Logger.getLogger(hw.class);
-		//logger.info("Test mvn");
+		final Logger logger=Logger.getLogger(hw.class);
+		logger.info("Test mvn");
 		System.out.println("=======Test ====");
 	}
 
